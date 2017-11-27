27bf18d3c1df83ec37d3e0725c31e98d98cc3876
AnuprasV <anupras@gmail.com>
2017-11-27 13:17:42 +0200

    keitimas

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ function abortInit()
   print("Press ENTER to abort startup")
   uart.on("data"�, "\r"�,
     function(data)
-      tmr.unregister(0) — disable the start up timer
-      uart.on(“data”) — stop capturing the uart
-        print(‘Startup aborted’)
+      tmr.unregister(0)      -- disable the start up timer
+      uart.on("data"�)        -- stop capturing the uart
+        print("Startup aborted")
     end, 0)
     tmr.alarm(0,5000,0, function()
-      uart.on(“data”) — stop capturing the uart
-        print(‘Running startup’)
-        dofile(‘wireless.lua’) — run the main program
+      uart.on("data"�)        -- stop capturing the uart
+        print("Running startup")
+        dofile("wireless.lua")  -- run the main program
     end)
 end
 
