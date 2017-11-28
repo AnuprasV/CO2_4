cc5ff89ed7f8c0d0190aa9a1301d8746c68e5fde
AnuprasV <34024020+AnuprasV@users.noreply.github.com>
2017-11-28 13:43:19 +0200

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,7 +1,7 @@
 t = 0
 
 function sendt(t,co,key)
- print("Temp:"..t.." C\n")
+-- print("Temp:"..t.." C\n")
 conn=net.createConnection(net.TCP, 0) 
 conn:on("receive", function(conn, payload) print(payload) end)
 conn:connect(80,'184.106.153.149') 
@@ -12,11 +12,11 @@ conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n
 conn:send("\r\n")
 
 conn:on("sent",function(conn)
-                    print("Closing connection")
+--                    print("Closing connection")
                       conn:close()
                   end)
 conn:on("disconnection", function(conn)
-                              print("Got disconnection...")
+--                              print("Got disconnection...")
   end)
 end
 
@@ -44,4 +44,4 @@ t = read()
 uart.write(0,0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79)
 end
 
-tmr.alarm(0, 60000, 1, function() sendData() end )
+tmr.alarm(5, 60000, 1, function() sendData() end )
